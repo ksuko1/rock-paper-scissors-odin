38e13c3624f5956c7e57107ddb306588acd4306d
Karl <139282956+ksuko1@users.noreply.github.com>
2024-07-13 17:14:12 +0100

    return computer choice
    function created to randomly generate computer choice (ROCK PAPER SCISSORS)

--- rps.js
+++ rps.js
@@ -1,1 +1,15 @@
-console.log("hello world")
+// Create a new function named getComputerChoice.
+function getComputerChoice() {
+    let randomNumber = Math.random()
+        if (randomNumber <= 0.33){
+        return "rock";
+        } else if (randomNumber <= 0.66){
+        return "paper";
+        } else {
+        return "scissors";
+        }
+}
+console.log(getComputerChoice())
+// // Test that your function returns what you expect using 
+// console.log or the browser developer tools before advancing 
+// to the next step.
